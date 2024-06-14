36922526b53a48119988f39a915840b3743c70b2
Ananya Jamdagneya <Ajamdagneya@gmail.com>
2024-06-14 12:00:44 -0500

    Initial Commit

---
+++ Anything/AnythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnythingApp.swift
+//  Anything
+//
+//  Created by Scholar on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AnythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anything/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Anything
+//
+//  Created by Scholar on 6/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Ananya and Audrey!")
+                .foregroundColor(Color.orange)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
